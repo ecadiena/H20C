3a3bbf86af60133ce0c9b5ccac3e14c4f0d7deac
Ethan Chee <ethancheez@gmail.com>
2022-10-16 23:57:35 -1000

    Fix add stuff page testcafe

--- app/imports/ui/pages/AddStuff.jsx
+++ app/imports/ui/pages/AddStuff.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import CreateSession from '../components/session/CreateSession';
+import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
 const AddStuff = () => (
-  <CreateSession />
+  <CreateSession id={PAGE_IDS.ADD_STUFF} />
 );
 
 export default AddStuff;

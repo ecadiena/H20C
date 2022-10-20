44f8a7c949532fd4a0735df702137a25e84b5649
Ethan Chee <ethancheez@gmail.com>
2022-10-20 13:57:36 -1000

    Change navbar styling

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -13,10 +13,11 @@ const NavBar = () => {
   const { currentUser } = useTracker(() => ({
     currentUser: Meteor.user() ? Meteor.user().username : '',
   }), []);
-  const menuStyle = { paddingTop: '20px', paddingBottom: '20px', marginBottom: '40px', borderBottom: '0.1px solid #D6D8DA' };
-  const itemStyle = { paddingLeft: '30px', paddingRight: '30px', color: '#1762A7', fontWeight: 'bold' };
+  const menuStyle = { paddingTop: '10px', paddingBottom: '10px', marginBottom: '40px', borderBottom: '0.1px solid #D6D8DA' };
+  const itemStyle = { paddingRight: '40px', color: '#1762A7', fontWeight: 'bold' };
+  const rightItemStyle = { color: '#1762A7', fontWeight: 'bold' };
   return (
-    <Navbar style={menuStyle}>
+    <Navbar style={menuStyle} expand="md">
       <Container>
         <Navbar.Toggle aria-controls={COMPONENT_IDS.NAVBAR_COLLAPSE} />
         <Navbar.Collapse id={COMPONENT_IDS.NAVBAR_COLLAPSE}>
@@ -25,16 +26,16 @@ const NavBar = () => {
             <Nav.Link style={itemStyle} id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} to="/list" key="list">About</Nav.Link>
             <Nav.Link style={itemStyle} id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} to="/list" key="list">Resources</Nav.Link>
           </Nav>
-          <Navbar.Brand className="ms-auto justify-content-center" id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/">
-            <img src="/images/uh-logo.png" alt="University of Hawaii Logo" />
+          <Navbar.Brand className="ms-auto justify-content-center" id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/" style={{ width: '20%' }}>
+            <img className="img-fluid" src="/images/uh-logo.png" alt="University of Hawaii Logo" />
           </Navbar.Brand>
           <Nav className="ms-auto justify-content-end">
             {currentUser === '' ? ([
-              <Nav.Link style={itemStyle} id={COMPONENT_IDS.NAVBAR_SIGN_IN_UP} as={NavLink} to="/sign-in-up" key="sign-in-up">
+              <Nav.Link style={rightItemStyle} id={COMPONENT_IDS.NAVBAR_SIGN_IN_UP} as={NavLink} to="/sign-in-up" key="sign-in-up">
                 Sign in / Sign up
               </Nav.Link>,
             ]) : ([
-              <NavDropdown style={itemStyle} id={COMPONENT_IDS.NAVBAR_CURRENT_USER} title={currentUser}>
+              <NavDropdown style={rightItemStyle} id={COMPONENT_IDS.NAVBAR_CURRENT_USER} title={currentUser}>
                 <NavDropdown.Item style={itemStyle} id={COMPONENT_IDS.NAVBAR_ACCOUNT} as={NavLink} to="/profile-page"><Person /> My Profile</NavDropdown.Item>
                 <NavDropdown.Item style={itemStyle} id={COMPONENT_IDS.NAVBAR_SIGN_OUT} as={NavLink} to="/signout"><BoxArrowRight /> Sign out</NavDropdown.Item>
               </NavDropdown>,

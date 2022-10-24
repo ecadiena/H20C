cba08bd0928557ffeb9264d29ffcb4602e060180
alvinedrada <aedrada@hawaii.edu>
2022-10-23 22:00:35 -1000

    Help button modal

--- app/imports/ui/components/HelpButton.jsx
+++ app/imports/ui/components/HelpButton.jsx
@@ -17,9 +17,22 @@ const HelpButton = () => {
       </Button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title><b>Frequently Asked Questions</b></Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <h4><b>What is Broadband for Hawaiʻi?</b></h4>
+          <p>Broadband for Hawaiʻi is text text text text text
+            <br />
+            Learn more in our <a href="/about">About page</a> and <a href="/resources">Resources page</a>.
+          </p>
+          <h4><b>How do I sign up for a class?</b></h4>
+          <p>To sign up you must first create an account at our <a href="/">Sign Up page</a>.
+            <br />You can then sign up for a class through the <a href="/">Classes page</a>.
+          </p>
+          <h4><b>Any other questions or site not functioning properly?</b></h4>
+          <p>Please send your question or describe the issue by emailing us at <a href="/">sample@email.com</a>
+          </p>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close

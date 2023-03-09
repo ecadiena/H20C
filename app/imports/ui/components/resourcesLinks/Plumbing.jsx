eb1a06f4f79315402476448266626207801a29c1
Edalaine Cadiena <ecadiena323@gmail.com>
2023-03-09 10:33:33 -1000

    edit resources page

---
+++ app/imports/ui/components/resourcesLinks/Plumbing.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+export const Plumbing = () => (
+  <Col fluid className="p-3 pb-0">
+    <h2>Plumbing-Heating-Cooling Contractors Association </h2>
+    <p><a href="https://www.ua.org/">https://www.ua.org/</a></p>
+    <p>
+      The United Association of Journeymen and Apprentices of the Plumbing and Pipefitting Industry of the United States and
+      Canada (UA) represents approximately 362,000 Plumbers, Pipefitters, Sprinkler Fitters, HVACR Service Technicians,
+      Welders and Pipeliners working in the construction industry throughout North America.
+    </p>
+
+    <h2>American Society of Plumbing Engineers</h2>
+    <p><a href="https://www.phccweb.org/">https://www.phccweb.org/</a></p>
+    <p>
+      The Plumbing-Heating-Cooling Contractors Association is dedicated to the advancement and education of the plumbing and
+      HVACR industry for the health, safety and welfare of society and the protection of the environment.
+    </p>
+
+    {/* eslint-disable-next-line react/no-unescaped-entities */}
+    <h2>Condominium Plumbing</h2>
+    <p><a href="https://aqueductplumbingcompany.com/">https://aqueductplumbingcompany.com/</a></p>
+    <p>
+      Our team of highly experienced plumbing contractors works hard to ensure our customers get not only the highest-quality
+      water into and throughout their homes, but we also ensure that their water stays where it’s supposed to be—in their pipes!
+    </p>
+
+    <h2>Plumbing Basics</h2>
+    <p><a href="https://www.thisoldhouse.com/plumbing/21128553/all-about-plumbing">https://www.thisoldhouse.com/plumbing/21128553/all-about-plumbing</a></p>
+    <p>
+      This Old House remains as one of the highest-rated home improvement show on television!
+    </p>
+
+    <h2>Plumbing 101</h2>
+    <p><a href="https://www.hometips.com/plumbing">https://www.hometips.com/plumbing</a></p>
+    <p>
+      HomeTips has been serving inspiration and expert, trusted DIY advice to homeowners through the Web, books, magazines, and
+      television for more than 25 years.
+    </p>
+
+    {/* eslint-disable-next-line react/no-unescaped-entities */}
+    <h2>A Beginner's Guide to Plumbing Codes</h2>
+    <p>
+      <a href="https://quintessentialplumbing.com.au/plumbing-knowledge-base/plumbing-codes-the-beginners-guide/">
+        https://quintessentialplumbing.com.au/plumbing-knowledge-base/plumbing-codes-the-beginners-guide/
+      </a>
+    </p>
+    <p>
+      Quintessential Plumbing team are among the top achievers in the industry and consistently renovate and maintain more
+      properties in the St George and Sydney area.
+    </p>
+  </Col>
+);
+
+export default Plumbing;
--- app/imports/ui/pages/Resources.jsx
+++ app/imports/ui/pages/Resources.jsx
@@ -1,45 +1,44 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Tabs, Tab, Container } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
+import Plumbing from '../components/resourcesLinks/Plumbing';
+
+const headingStyle = { fontSize: '48px', fontWeight: 600 };
+const subheadingStyle = { fontSize: '20px', fontWeight: 400, textAlign: 'center' };
 
 /* A simple static component to render some text for the landing page. */
-const Resources = () => {
-  const headingStyle = { fontSize: '48px', fontWeight: 600 };
-  const subheadingStyle = { fontSize: '20px', fontWeight: 400 };
-  return (
-    <div id={PAGE_IDS.ABOUT}>
-      <Row fluid className="justify-content-center mx-0">
-        <Col fluid md={7} className="p-3 pb-0">
-          <h1 className="text-center pb-2 mx-0" style={headingStyle}>Resources</h1>
-          <p style={subheadingStyle}>There are many more resources available for you to continue your exploration of broadband. Below are a list of websites and programs that can help you learn more about broadband and its benefits.
-          </p>
-          <h2>Federal Communications Commission (FCC) Affordable Connectivity Program</h2>
-          <p><a href="https://www.fcc.gov/acp">https://www.fcc.gov/acp</a></p>
-          <p>FCC&apos;s Affordable Connectivity Program allows households to get a discount of up to $30 per month toward internet service, and up to $100 to purchase a laptop, desktop computer, or tablet from participating providers.</p>
-          <h2>State of Hawaii DBEDT – Hawaii Broadband & Digital Equity Office</h2>
-          <p><a href="https://broadband.hawaii.gov/">https://broadband.hawaii.gov/</a></p>
-          <p>Taking a statewide, holistic approach to the Hawaii Broadband Initiative (HBI), DBEDT recognizes that broadband touches the entire community to provide the foundation for innovation,
-            economic development, healthcare, education, public safety, entertainment, etc. through the entire continuum of broadband connectivity.
-          </p>
-          <h2>National Broadband Resource Hub</h2>
-          <p><a href="https://www.broadbandhub.org/">https://www.broadbandhub.org/</a></p>
-          <p>The National Broadband Resource Hub (NBRH) is a free online community for government leaders and nonprofits working to expand broadband access and affordability to build a strong digital future. </p>
-          <h2>BroadbandUSA National Telecommunications and Information Administration</h2>
-          <p><a href="https://broadbandusa.ntia.doc.gov/">https://broadbandusa.ntia.doc.gov/</a></p>
-          <p>The National Telecommunications and Information Administration (NTIA), located within the Department of Commerce,
-            is the Executive Branch agency principally responsible by law for advising the President on telecommunications and information policy issues.
-            NTIA’s programs and policymaking focus largely on expanding broadband Internet access and adoption in America,
-            expanding the use of spectrum by all users, and ensuring that the Internet remains an engine for continued innovation and economic growth
-          </p>
-          <h2>Cable Television (CATV) Division of the Department of Commerce and Consumer Affairs (DCCA) Broadband Webpage</h2>
-          <p><a href="https://cca.hawaii.gov/broadband/">https://cca.hawaii.gov/broadband/</a></p>
-          <p>The Cable Television Division works to promote and facilitate access to affordable, broadband-level telecommunications service across the State so that all residents
-            may benefit from the many online applications, programs, and resources available, including telework, e-learning programs, and online job, health, and government services.
-          </p>
-        </Col>
-      </Row>
-    </div>
-  );
-};
+const Resources = () => (
+  <Container id={PAGE_IDS.ABOUT}>
+    <h1 className="text-center pb-2 mx-0" style={headingStyle}>Resources</h1>
+    <p style={subheadingStyle}>
+      Below is a list of websites and programs that can help you learn more about condo maintenance and its benefits.
+    </p>
+    <Tabs
+      defaultActiveKey="plumbing"
+      id="justify-tab-example"
+      className="mb-3"
+      justify
+    >
+      <Tab eventKey="plumbing" title="Plumbing">
+        <Plumbing />
+      </Tab>
+      <Tab eventKey="hvac" title="HVAC">
+        <Plumbing />
+      </Tab>
+      <Tab eventKey="fire-prevention" title="Fire Prevention">
+        <Plumbing />
+      </Tab>
+      <Tab eventKey="state-mandates" title="State Mandates">
+        <Plumbing />
+      </Tab>
+      <Tab eventKey="safety-regulations" title="Safety Regulations">
+        <Plumbing />
+      </Tab>
+      <Tab eventKey="insurance" title="Insurance">
+        <Plumbing />
+      </Tab>
+    </Tabs>
+  </Container>
+);
 
 export default Resources;
